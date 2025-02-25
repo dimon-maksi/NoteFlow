1dc073a354f90f0920ffe3e16cf1405af049a7d3
DESKTOP-6IN3NEQ\LEGION <nazarhuyk@gmail.com>
2025-02-25 14:45:17 +0200

    chore: dependencies for the backend

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # NoteFlow
 Student Note Management System
+
+NoteFlow is a web-based platform that allows users to create, edit, and manage notes collaboratively or individually.
+This project provides a server-side API built on C# (.NET Core)
+that supports user authentication, real-time collaboration, rich text editing, and media embedding
--- backend/NoteFlowAPI/Program.cs
+++ backend/NoteFlowAPI/Program.cs
@@ -1,6 +1,33 @@
+using dotenv.net;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using MongoDB.Driver;
+using System.Text;
+
+DotEnv.Load();
+var configuration = new ConfigurationBuilder().AddEnvironmentVariables().Build();
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<IMongoClient, MongoClient>(_ => new MongoClient(configuration["MONGO_CONNECTION_STRING"]));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = configuration["JWT_ISSUER"],
+            ValidAudience = configuration["JWT_AUDIENCE"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT_SECRET"]))
+        };
+    });
+
 builder.Services.AddOpenApi();
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -11,11 +38,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
 app.MapGet("/", () =>
 {
     return "Hello World!";
 })
 .WithName("NoteFlow");
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
