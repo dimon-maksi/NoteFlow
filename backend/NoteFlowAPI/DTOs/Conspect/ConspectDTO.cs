85e0693301009f639276ceecb68507702dc49f34
maglada <vitaliioperrenko@gmail.com>
2025-04-28 16:50:39 +0300

    add IsPrivate and new table. edit consprctDTO

--- backend/NoteFlowAPI/DTOs/Conspect/ConspectDTO.cs
+++ backend/NoteFlowAPI/DTOs/Conspect/ConspectDTO.cs
@@ -8,4 +8,6 @@ public class ConspectDTO
     public string? Description { get; set; }
     public string? Text { get; set; }
     public DateTime DateTime { get; set; }
+    public bool IsPrivate { get; set; }
 }
+
--- backend/NoteFlowAPI/Models/Conspect.cs
+++ backend/NoteFlowAPI/Models/Conspect.cs
@@ -10,26 +10,33 @@ public class Conspect
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
-    
+
     [Required]
     [StringLength(300)]
     [BsonElement("title")]
     public required string Title { get; set; }
-    
+
     [Required]
     [StringLength(300)]
     [BsonElement("type")]
     public required string Type { get; set; }
-    
+
     [BsonElement("description")]
     public string? Description { get; set; }
-    
+
     [BsonElement("text")]
     public string? Text { get; set; }
-    
+
     [BsonElement("dateTime")]
     public DateTime DateTime { get; set; }
-    
+
     [BsonElement("userId")]
     public string? UserId { get; set; }
+
+    [Required]
+    public bool IsPrivate { get; set; } = false;
+
+    [BsonElement("allowedUsers")]
+    public List<Guid> AllowedUserIds { get; set; } = new List<Guid>();
 }
+
